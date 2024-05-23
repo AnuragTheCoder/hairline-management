6bac669b635d7d25765fefaba2f95c149a4980c4
AnuragTheCoder <129210052+AnuragTheCoder@users.noreply.github.com>
2024-05-24 00:56:36 +0530

    firts

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+
+const path = require("path");
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config({ path: "./config/config.env" });
+}
+
+// Using Middlewares
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
+
+module.exports = app;
